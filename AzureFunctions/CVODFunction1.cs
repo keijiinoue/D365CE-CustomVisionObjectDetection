f35993637d034d8c5a18a3089fbfb4804e534e1b
keijiinoue <keiji.inoue@keiji.org>
2018-08-11 09:44:03 +0900

    Added files

---
+++ AzureFunctions/CVODFunction1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+using Newtonsoft.Json;
+
+namespace CustomVisionObjectDetectionFunction1
+{
+    /// <summary>
+    /// 画像データを受け取って Custom Vision Object Detection を利用して検知し、検知した画像を処理した画像を返す。
+    /// 検知のパーセンテージ数値のしきい値は設けていない。
+    /// Custom Vision Object Detection を 専用APIでコールする。
+    /// D365 CE の JavaScript Web Resource から呼び出されることを想定している。
+    /// 
+    /// POSTで受け取るデータは、以下のような JSON データを想定している。
+    /// {
+    ///   "mimetype": "image/jpeg",
+    ///   "documentbody": "/9j/4AAQSkZJRgABAQEASABIAAD/4SlMRXhpZgAATU0AKgAAAAgADQALAAIAAAAmAAAItgEP・・・省略・・・345orOTFoz/2Q=="
+    /// }
+    /// 
+    /// 戻り値はBASE64形式の画像データで、mimetype は入力時と変わらない。
+    /// 
+    /// Function6bとの違いは、fontsizeWidthRatio をパラメーターとして受け取ることだけ。
+    /// 
+    /// </summary>
+    public static class CVODFunction1
+    {
+        #region Settings for YOUR Custom Vision Object Detection
+        const string projectId = "xxxxxxxxxxx"; // Prediction URL の Prediction/ の後に続く/までの文字列
+        const string iterationId = "xxxxxxxxxxx"; // Prediction URL の iterationId= の後に続く文字列
+        const string predictionKey = "xxxxxxxxxxxx"; // ヘッダー Prediction-Key に設定すべきと記載のある文字列
+        #endregion
+        static float fontsizeWidthRatio = 0.02f; // 画像の幅に対するフォントサイズの比率の既定値。この数値が大きいほど、フォントサイズが大きくなる。
+        [FunctionName("CVODFunction1")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            string jsonContent = await req.Content.ReadAsStringAsync();
+            //log.Info(jsonContent);
+
+            dynamic data = JsonConvert.DeserializeObject(jsonContent);
+            string mimetype = data.mimetype;
+            string documentbody = data.documentbody; // BASE64の画像データ
+            fontsizeWidthRatio = data.fontsizeWidthRatio;
+
+            log.Info($"documentbody.Length: {documentbody.Length}, mimetype: {mimetype}");
+
+            if (documentbody.Length > 0 && mimetype != "")
+            {
+                try
+                {
+                    IList<PredictionModel> predictions = GetPredictions(documentbody, log);
+                    string base64Image = GetPredictedBase64Image(documentbody, mimetype, predictions, log);
+                    log.Info($"Got base64Image. base64Image.Length: {base64Image.Length}");
+
+                    return req.CreateResponse(HttpStatusCode.OK, base64Image);
+                }
+                catch (Exception ex)
+                {
+                    log.Info($"Error {ex.Message}");
+
+                    return req.CreateResponse(HttpStatusCode.OK, "Error {ex.Message}");
+                }
+            }
+            else
+            {
+                log.Info($"Exiting as not matching for (documentbody.Length > 0 && mimetype != \"\")");
+
+                return req.CreateErrorResponse(HttpStatusCode.OK, "Exiting as not matching for (documentbody.Length > 0 && mimetype != \"\")");
+            }
+        }
+
+        /// <summary>
+        /// 検知した結果を表す描画を施した画像の Base64 文字列を返す。
+        /// 上手くいかなかった場合には、""を返す。
+        /// </summary>
+        /// <param name="documentbody"></param>
+        /// <param name="predictions"></param>
+        /// <returns></returns>
+        private static string GetPredictedBase64Image(string documentbody, string mimetype, IList<PredictionModel> predictions, TraceWriter log)
+        {
+            Image image;
+            using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(documentbody)))
+            {
+                image = Image.FromStream(ms);
+                Bitmap bitmap = new Bitmap(image);
+                log.Info($"Got bitmap. bitmap.Width: {bitmap.Width}, bitmap.Height: {bitmap.Height}");
+
+                Graphics g = Graphics.FromImage(image);
+                // 検出した領域を示すための矩形の色のリスト
+                List<Color> colors = new List<Color> {
+                    Color.Blue,
+                    Color.Red,
+                    Color.Purple,
+                    Color.Green,
+                    Color.Cyan,
+                    Color.Yellow
+                };
+                // 降順にソート
+                List<PredictionModel> sortedPredictions = predictions.OrderByDescending(p => p.Probability).ToList();
+
+                for (int i = 0; i < sortedPredictions.Count; i++)
+                {
+                    var prediction = sortedPredictions[i];
+                    var color = colors[i % colors.Count];
+                    Pen p = new Pen(color, bitmap.Width / 200);
+                    // 検出した領域の矩形
+                    g.DrawRectangle(
+                        p,
+                        bitmap.Width * (float)prediction.BoundingBox.Left,
+                        bitmap.Height * (float)prediction.BoundingBox.Top,
+                        bitmap.Width * (float)prediction.BoundingBox.Width,
+                        bitmap.Height * (float)prediction.BoundingBox.Height);
+                    Font font = new Font("Segoe UI", bitmap.Width * fontsizeWidthRatio);
+                    string text = $"{prediction.TagName}: {prediction.Probability:P1}";
+                    // 文字列の描画サイズ
+                    SizeF textSize = g.MeasureString(text, font);
+                    // 文字列のための背景の矩形（その場所と左上の2か所）
+                    g.FillRectangle(
+                        new SolidBrush(color),
+                            bitmap.Width * (float)prediction.BoundingBox.Left,
+                            bitmap.Height * (float)(prediction.BoundingBox.Top + prediction.BoundingBox.Height),
+                            textSize.Width,
+                            textSize.Height);
+                    g.FillRectangle(
+                        new SolidBrush(color),
+                            0,
+                            i * textSize.Height,
+                            textSize.Width,
+                            textSize.Height);
+                    // 文字列（その場所と左上の2か所）
+                    g.DrawString(
+                        text,
+                        font,
+                        Brushes.White,
+                        bitmap.Width * (float)prediction.BoundingBox.Left,
+                        bitmap.Height * (float)(prediction.BoundingBox.Top + prediction.BoundingBox.Height));
+                    g.DrawString(
+                        text,
+                        font,
+                        Brushes.White,
+                        0,
+                        i * textSize.Height);
+                }
+            }
+            if (image != null)
+            {
+                log.Info("Got predicted image");
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    image.Save(ms, getImageFormatFromMIMEtype(mimetype));
+                    log.Info("Image saved");
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+
+            return "";
+        }
+        private static System.Drawing.Imaging.ImageFormat getImageFormatFromMIMEtype(string mimetype)
+        {
+            switch (mimetype)
+            {
+                case "image/gif":
+                    return System.Drawing.Imaging.ImageFormat.Gif;
+                case "image/png":
+                    return System.Drawing.Imaging.ImageFormat.Png;
+                case "image/jpeg":
+                    return System.Drawing.Imaging.ImageFormat.Jpeg;
+                case "image/bmp":
+                    return System.Drawing.Imaging.ImageFormat.Bmp;
+                default:
+                    throw new Exception($"mimetype '{mimetype}' is not supported by this function.");
+            }
+        }
+        private static IList<PredictionModel> GetPredictions(string documentbody, TraceWriter log)
+        {
+            PredictionEndpoint endpoint = new PredictionEndpoint() { ApiKey = predictionKey };
+            log.Info("Making prediction(s):");
+            IList<PredictionModel> predictions;
+
+            using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(documentbody)))
+            {
+                var result = endpoint.PredictImage(new Guid(projectId), ms, iterationId: new Guid(iterationId));
+                predictions = result.Predictions;
+
+                foreach (var p in result.Predictions)
+                {
+                    log.Info($"\t{p.TagName}: {p.Probability:P1} [ {p.BoundingBox.Left}, {p.BoundingBox.Top}, {p.BoundingBox.Width}, {p.BoundingBox.Height} ]");
+                }
+            }
+            log.Info("Got prediction(s):");
+
+            return predictions;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # D365CE-CustomVisionObjectDetection
-D365 CE and Custom Vision Object Detection
+D365 CE と Custom Vision Object Detection (執筆時点でプレビュー) の API を使ったサンプルです。  
+D365 CE と Custom Vision Object Detection と Azure Functions を利用しています。
+
+## 前提条件
+- Custom Vision の Object Detection で Training が済んでいる Project をお持ちであること。
+    * 注意：Custom Vision Object Detection は2018年8月9日時点でプレビュー機能です。
+- Azure 環境をお持ちであること
+- D365 CE 環境ををお持ちであること
+    * D365 CE v9.0.2 の環境で開発しています。
+
+## 動作の流れ
+1. D365 CE の Web リソースが画像を取得し、必要に応じて圧縮し、Azure Functions の関数に渡す。
+1. Azure Functions の関数は、Custom Vision Object Detection の API を呼び出し、検知した情報を取得。その情報に応じて検知された内容が内部に記載された画像を生成し、返す。
+1. D365 CE の Web リソースが Azure Functions の関数から検知された内容が内部に記載された画像を受け取り、フォーム上に表示。同時に2つのメモレコードを作成する。
+
+## サンプル展開手順
+1. Custom Vision Object Detection の設定
+    1. Training が済んでいる Project の画面に行き、Performance をクリック
+    1. Prediction URL をクリック
+    1. "If you have an image file:" の方に表示された URL などの情報をメモしておく（※1）
+1. Azure Functions の設定
+    1. VS で Azure Functions プロジェクトを新規に作成
+    1. そのプロジェクトに NuGet パッケージを適用
+    1. 新規の関数を作成し、[このフォルダ](AzureFunctions) にある「CVODFunction1.cs」を、※1の内容を基に、projectId、iterationId、predictionKey を適宜修正
+    1. Azure に発行
+    1. Azure ポータル上で、その Function App における「プラットフォーム機能」内の CORS 設定で D365 CE インスタンスの URL を指定
+    1. Azure ポータル上で、その Function App 内の CVODFunction1 に遷移し、「</> 関数の URL の取得」をクリックして、URL をコピーしておく（※2）
+1. D365 CE の Web リソースの設定
+    1. [このフォルダ](D365CE_Solution) にある IncidentObjectDetection_1_0_0_0_p.zip あるいは IncidentObjectDetection_1_0_0_0_managed_p.zip  を D365 CE インスタンスにインポート
+        - このソリューションは、専用の1つのアプリを追加するものです。なお、サポート案件 エンティティを使用し、フォームを追加しているがフィールドは追加していません。
+    1. main.js を編集する。内の Detector.AzureFunctionUri の値として、上記※2の値を貼り付けます。
+    1. すべてのカスタマイズを公開します。
+
+## サンプル利用手順
+1. D365 CE の「サポート案件オブジェクト検知」アプリ（Unified Interface）をオープンします。PC からでもモバイル アプリからでも利用できます。
+1. 既存の（新規ではない）サポート案件レコードを開きます。
+1. サポート案件フォーム「オブジェクト検知」が表示されていることを確認します。
+    - なお、必要に応じてソリューションファイル内の html ファイルを編集して「パンダとキリンの検知」のような文字列は適宜ご自身の Custom Vision の Project の内容に応じて修正ください。なおこのサンプルでは、この select 要素のメニューはプログラムのロジックには何も影響を与えていないものです。
+    - ![Img001](imgs/Img001.png)
+1. フォーム上左側の「オブジェクト検知」Web リソース 表示エリア内で「ファイルを選択」をクリックし、既存の画像ファイルを選択するか、その場で写真を撮ってください。
+    - ![Img002](imgs/Img002.png)
+    - ![Img003](imgs/Img003.png)
+1. 「検知する」ボタンをクリックください。
+    - ![Img004](imgs/Img004.png)
+1. 検出された内容が内部に記載された画像が表示されます。
+    - ![Img005](imgs/Img005.png)
+1. 同時に、このサポート案件レコードに紐づく2つのメモレコードが作成されています。（タイムラインの表示を最新のものに更新する必要があります。）
+    - 1つは検出された内容が内部に記載された画像ファイルを持つメモレコード
+    - もう1つは、オリジナルの画像ファイル（圧縮前）を持つメモレコード
+    - ![Img006](imgs/Img006.png)
+
+## より現実的に使用するために考えられること
+- このサンプルでは、Custom Vision Object Detection で検知した結果は画像ファイル内にのみ情報が存在しています。検知した結果をもとに何かしらの処理を D365 CE 上で自動化したいような場合には、検知結果は画像だけでなく、データとしても返すような仕組みが有効と考えられます。そのためには、Azure Functions 関数を修正する、また D365 CE 側の Web リソースがそれを扱えるように修正することが必要です。
